69a80ad2e5006d60af2274c7295a6b113894f0fe
Guillaume Lahaie <glahaie@gmail.com>
2014-04-11 21:28:39 -0400

    Code python tout fait

--- monnaie.py
+++ monnaie.py
@@ -7,13 +7,21 @@
     LAHG04077707
     Dernière mise à jour: 10 avril 2014
 
-    Implémentation de l'algorithme de monnaie
+    Implémentation de l'algorithme de monnaie. Pour le moment je suppose
+    qu'il est possible de rendre la monnaie pour tous les montants possibles
+    il faudrait trouver traiter le cas où il n'est pas possible de rendre
+    la monnaie
 """
 
 import sys
 
 
+
 def afficherC(L, P):
+    """
+        Affiche toutes les lignes de l'algorithme de programmation dynamique permettant
+        de trouver le nombre de pièces optimales à rendre pour L
+    """
     C = [-1]*(L+1)
     for j in range(0, len(P)):
         C[0] = 0
@@ -26,6 +34,87 @@ def afficherC(L, P):
                         min = temp
             C[i] = min
         #On imprime la ligne
-        print reduce(lambda x, y: x + " " + y, map(lambda x: str(x) + " ", C))
+        print reduce(lambda x, y: x + " "+y,
+                     map(lambda x: "{"+str(x)+":3}",
+                         range(0, len(C)))).format(*C).strip()
 
-        
+def calculerC(M, P):
+    """On retourne la dernière ligne de C pour la plus grande valeur de
+       contenue dans M
+    """
+
+    maxChange = reduce(lambda x, y: max(x, y), M)
+    C = [-1]*(maxChange+1)
+    for j in range(0, len(P)):
+        C[0] = 0
+        for i in range(1, maxChange+1):
+            min = sys.maxint
+            for k in range(0, j+1):
+                if P[k] <= i:
+                    temp = 1 + C[i-P[k]]
+                    if temp < min:
+                        min = temp
+            C[i] = min
+
+    return C
+
+
+def lireFichier(nom_fichier):
+    """Lit le fichier à traiter: la première contient le nombre de valeurs de
+       pièces de monnaie, ensuite les valeurs des pièces de monnaie, le
+       nombre de valeurs à calculer, finalement les valeurs de monnaie à rendre
+    """
+    with open(nom_fichier, "r") as f:
+        no_pieces = int(f.readline().strip())
+        P = map(lambda x: int(x),
+                filter(lambda x: x != "",
+                       map(lambda x: x.strip(),
+                           f.readline().strip().split(" "))))
+
+        assert (len(P) == no_pieces),"Erreur de dimensions dans le fichier " + nom_fichier
+        no_monnaie = int(f.readline().strip())
+        Monnaies = map(lambda x: int(x),
+                filter(lambda x: x != "",
+                       map(lambda x: x.strip(),
+                           f.readline().strip().split(" "))))
+        assert (len(Monnaies) == no_monnaie),"Erreur de dimensions dans le fichier " + nom_fichier
+
+    return P, Monnaies
+
+def trouverMonnaieOptimale(C, monnaie, P):
+
+    S = [0]*len(P)
+    if C[monnaie] < 0 or C[monnaie] == sys.maxint:
+        return map(lambda x: -1, S)
+    i = 0
+    n = len(P)
+    while monnaie > 0:
+        if (monnaie >= P[n-1-i]) and (C[monnaie] > C[monnaie - P[n-1-i]]):
+            monnaie = monnaie - P[n-1-i]
+            S[i] += 1
+        else:
+            i += 1
+    return S
+
+
+
+def main():
+
+    fichier_defaut = "monnaie.txt"
+    if len(sys.argv) > 1:
+        fichier_defaut = sys.argv[1]
+
+    P, Monnaies = lireFichier(fichier_defaut)
+
+    #On imprime pour L = 20
+    print reduce(lambda x, y: str(x) + " " + str(y), P)
+    afficherC(20, P)
+
+    C = calculerC(Monnaies, P)
+
+    print "\nMontant: nombre de pièces en ordre décroissant de valeur"
+    for i in Monnaies:
+        print str(i) + " : " + reduce(lambda x, y: str(x) + " " + str(y), trouverMonnaieOptimale(C, i, P))
+
+if __name__ == "__main__":
+        main()
